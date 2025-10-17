8312cee20d8638a7285c26e0f5dc15bff61c4942
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2025-10-17 15:31:49 +0100

    header link animation

--- src/app/components/HeaderLink.tsx
+++ src/app/components/HeaderLink.tsx
@@ -8,8 +8,12 @@ export default function HeaderLink({
   children: React.ReactNode;
 }) {
   return (
-    <Link href={href} className='hover:text-mf-green transition-colors'>
+    <Link
+      href={href}
+      className='relative hover:text-mf-green transition-colors duration-300 group'
+    >
       {children}
+      <span className='absolute bottom-0 left-0 w-0 h-0.25 bg-mf-green transition-all duration-300 group-hover:w-full'></span>
     </Link>
   );
 }
