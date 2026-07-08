1d2fb2b87cee0598216bafe1ce4ec1c0958abafa
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2026-07-08 23:30:36 +0100

    Add Google Ads gtag.js tracking tag to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import HeaderWrapper from './components/HeaderWrapper';
 import { getSiteSettings } from './lib/sanity';
 import { generateMetadata as generateSiteMetadata } from './utils/metadata';
 import { StructuredData } from './components/StructuredData';
+
+const GTAG_ID = process.env.NEXT_PUBLIC_GTAG_ID || 'AW-18020600681';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,6 +31,21 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GTAG_ID}`}
+          strategy='afterInteractive'
+        />
+        <Script id='gtag-init' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GTAG_ID}');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
